19906a7cb5a257ab60ccad42c96f2da0ba0d98b2
Yamada Satoshi <atmonad@ring>
2012-07-31 23:12:57 +0900

    modify rss.rb

--- plugins/rss.rb
+++ plugins/rss.rb
@@ -1,11 +1,9 @@
 require 'rss'
 require 'open-uri'
 require 'digest/sha2'
-require 'logger'
 
 class FeedReader
   def initialize(url, min_time = Time::at(0))
-#    @min_time = min_time
     @url = url
     @hashes = []
   end
@@ -17,26 +15,18 @@ class FeedReader
       return :error
     end
     @rss.output_encoding = "UTF-8"
-    log.debug("RSS Updated: #{@hashes.map{|s| s.unpack('H*')[0][0..10]}}")
+#    log.debug("RSS Updated: #{@hashes.length}")
 
     rss_items = []
-#    pmax_time = @min_time
     @rss.items.each do |item|
-      item_hash = Digest::SHA512.digest(item.inspect)
-      log.debug("New Hash: #{item_hash.unpack('H*')[0][0..10]}")
+      item_hash = Digest::SHA512.digest(item.link.inspect)
+#      log.debug("Hash: #{@hashes.include?(item_hash) ? 'Included' : 'New!' } (#{item_hash.unpack('H*')[0][0..5]})#{item.title[0..10]}")
       unless @hashes.include?(item_hash) then
         rss_items.push item
         @hashes.push item_hash
         @hashes.shift if @hashes.length > 1000
       end
-#      log.debug("Items show: #{item.inspect}")
-#      if item.date > @min_time then
-#        rss_items.push item
-#        pmax_time = pmax_time < item.date ? item.date : pmax_time
-#      end
     end
-#    @min_time = pmax_time
-#    log.debug("Updated time border: #{@min_time}")
 
     rss_items
   end
@@ -100,7 +90,7 @@ class RSSBot < Btmonad::Bot
         items = items.sort{|a, b| b.date <=> a.date}[0..9].reverse
       end
       items.each do |item|
-        s = item.title + " " + p.abbrurl(item.link)
+        s = item.title[0..250] + " " + p.abbrurl(item.link)
         p.privmsg s.tojis
       end
     end
